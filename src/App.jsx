5e12ec09ebbe854d67b7822551ba54bd347f9fd1
tavcoder <tarteagadesign@gmail.com>
2025-07-31 08:36:08 +0200

    refactor: update main.jsx and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Timeline from "./pages/Timeline";
@@ -7,18 +7,20 @@ import Messages from "./pages/Messages";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <main className="layout__main">
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/timeline" element={<Timeline />} />
-          <Route path="/people" element={<People />} />
-          <Route path="/messages" element={<Messages />} />
-        </Routes>
-      </main>
-    </Router>
+    <BrowserRouter >
+      <div className="layout">
+        <Navbar />
+        <main className="layout__main">
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/timeline" element={<Timeline />} />
+            <Route path="/people" element={<People />} />
+            <Route path="/messages" element={<Messages />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Fallback({ error }) {
+  return (
+    <div role="alert">
+      <p>Something went wrong:</p>
+      <pre style={{ color: "red" }}>{error.message}</pre>
+    </div>
+  );
+}
+
+const queryClient = new QueryClient();
+
+function AppSetup() {
+  return (
+    <StrictMode>
+      <ErrorBoundary FallbackComponent={Fallback}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ErrorBoundary>
+    </StrictMode>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<AppSetup />);
