9797ebd046c66ff827a44449d3c98e352fda30d1
Tania Arteaga <tarteagadesign@gmail.com>
2025-09-21 15:26:27 +0200

    fix: remove unused user variable from Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
 // src/components/Home.jsx
-import { useContext } from "react";
-import { AuthContext } from "@/context";
 import { PostList, NewPostForm  } from "@/components/post";
 
 function Home() {
-    const { user } = useContext(AuthContext);
 
     return (
         <section>
