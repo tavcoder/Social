6ea313a11601dd787d9769ef7b274ade152e22d6
Tania Arteaga <tarteagadesign@gmail.com>
2025-09-25 12:13:16 +0200

    refactor: move EditUserProfile to pages root and clean up structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as AuthPage } from "./AuthPage/index";
-export { default as EditUserProfile } from "./EditUserProfile/index";
+export { default as EditUserProfile } from "./EditUserProfile";
 export { default as Home } from "./Home";
 export { default as MainLayout } from "./MainLayout/index";
 export { default as People } from "./People";
